9ebbf5c8e2170da9174fc12b8653613464eb1d81
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-07-22 10:53:15 -0400

    can now add words to list

--- src/resources/elements/words.ts
+++ src/resources/elements/words.ts
@@ -2,11 +2,15 @@ import { bindable } from 'aurelia-framework';
 
 export class Words {
   @bindable private readonly words: string[] = ["am", "is", "are", "was", "were", "be", "being", "been"];
-
+  @bindable private readonly newWord: string;
   private Delete(word: string): void {
     const index = this.words.indexOf(word);
     if (index !== -1) {
       this.words.splice(index,1);
     }
+  }
+
+  private addOne(){
+    this.words.push(this.newWord);
   }
 }
