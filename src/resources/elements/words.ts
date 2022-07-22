8e241944d714341b890841f267a42e5e374064a7
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-07-22 08:18:37 -0400

    Improve immutability

--- src/resources/elements/words.ts
+++ src/resources/elements/words.ts
@@ -1,10 +1,10 @@
 import { bindable } from 'aurelia-framework';
 
 export class Words {
-  @bindable private words: string[] = ["am", "is", "are", "was", "were", "be", "being", "been"];
+  @bindable private readonly words: string[] = ["am", "is", "are", "was", "were", "be", "being", "been"];
 
   private Delete(word: string): void {
-    let index = this.words.indexOf(word);
+    const index = this.words.indexOf(word);
     if (index !== -1) {
       this.words.splice(index,1);
     }
